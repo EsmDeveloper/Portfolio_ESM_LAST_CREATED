955e0ef16b6737e5ad6a848fdcb1de2c827e409f
EsmDeveloper <esmeniosousa00@gmail.com>
2026-01-30 16:26:42 +0100

    fix MainLayout import

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './components/layout/MainLayout';
+
 import About from './pages/About/about';
 import Experience from './pages/Experience/Experience';
 import Skills from './pages/Skills/Skills';
